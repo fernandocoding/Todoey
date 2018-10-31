6f916caa78c9f63a9014f8ab807ac49adf174e9f
Fernando Rodriguez <fernando.coding@fernandos-macbook-air.local>
2018-10-31 07:57:18 -0700

    Convert to MVC design pattern

---
+++ Todoey/DataModel/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Fernando Rodriguez on 10/30/18.
+//  Copyright © 2018 Fernando Rodriguez. All rights reserved.
+//
+
+import Foundation
+
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
